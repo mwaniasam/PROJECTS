75eebd071adef6e82be64717abdf62b2424b25b0
mwaniasam <s.mwania@alustudent.com>
2026-03-04 20:48:29 +0530

    Clean hangman game. Let's play

--- learning_exercises/hangman/hangman_game.py
+++ learning_exercises/hangman/hangman_game.py
@@ -1,8 +1,9 @@
 # Guessing game
-# Two Players
+# One Player
 # Display the dashes according to the number of letters in the guessed word
 # Guess the letters, replace them in the word, if wrong, take one life away, if correct, let them keep
 # guessing. After six wrong guesses, GAME OVER!
+
 import random
 import hangman_stages
 import words
@@ -14,20 +15,37 @@ print("=" * 70)
 # hidden_word = ["Hobby", "Reckon", "Absolutely",
 #                "Philanthropy", "Physics", "Affection"]
 lives = 6
+
+# Generate random words from the words list
 chosen_word = random.choice(words.words).lower()
-print(chosen_word)
+# print(chosen_word)
 display = []
+guessed_words = []
 for i in chosen_word:
     display += '_'
-print(display)
+print(" ".join(display))
 game_over = False
 while not game_over:
     guessed_letter = input("Guess a letter: ").lower()
+    
+    # Let the user know that they already guessed that word
+    if guessed_letter in guessed_words:
+        print(f"You already guessed: {guessed_letter}")
+        continue
+    guessed_words.append(guessed_letter)
+    
+    # Input validation
+    if len(guessed_letter) != 1 or not guessed_letter.isalpha():
+        print("Please enter a single letter.")
+        continue
+        
+    # Checking whether the letter matches any word
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if guessed_letter == letter:
             display[position] = guessed_letter
-            print(display)
+            print(" ".join(display))
+
     if guessed_letter not in chosen_word:
         lives -= 1
         if lives == 0:
@@ -37,3 +55,6 @@ while not game_over:
         game_over = True
         print("You saved a man. YOU WIN!")
     print(hangman_stages.stages[lives])
+
+
+
