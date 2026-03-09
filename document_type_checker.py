0b49ac5906b37954595989ca53e9c70e8d5bb5c7
mwaniasam <s.mwania@alustudent.com>
2026-03-09 08:39:07 +0530

    Refactor by: Enabling case sensitivity handling, better loop breaking logic

--- document_type_checker.py
+++ document_type_checker.py
@@ -3,8 +3,7 @@ title_deed_words = [
     "title", "deed", "parcel", "land", "registered", "proprietor",
     "ownership", "tenure", "freehold", "leasehold",
     "survey", "boundary", "registry", "transfer",
-    "encumbrance", "charge", "easement", "allotment",
-    "certificate", "registration"
+    "encumbrance", "charge", "easement", "allotment"
 ]
 
 # Words commonly found in Kenyan court judgements
@@ -21,21 +20,23 @@ business_permit_words = [
     "permit", "license", "business", "premises",
     "county", "registration", "compliance", "inspection",
     "trading", "fee", "renewal", "tax",
-    "certificate", "authority", "application", "approval",
+    "authority", "application", "approval",
     "operator", "enterprise", "regulation", "validity"
 ]
-
 checking = True
 while checking:
-    word = input("What word do you want to check today?\n")
+    word = input("What word do you want to check today?\n").lower()
     if word in title_deed_words:
-        print(f"The word {word} shows your document is a title_deed.")
+        print(f"'{word}' suggests this is a Title Deed.")
     elif word in court_judgement_words:
-        print(f"The word {word} shows your document is a court judgement.")
+        print(f"'{word}' suggests this is a Court Judgement.")
     elif word in business_permit_words:
-        print(f"The word {word} shows your document is a business permit.")
+        print(f"'{word}' suggests this is a Business Permit.")
     else:
-        print(f"{word} unknown!")
+        print(f"'{word}' was not recognised.")
+
+    again = input("Check another word? (yes/no)\n").lower()
+    if again == "no":
         checking = False
-    break
+        print("Goodbye!")
 
